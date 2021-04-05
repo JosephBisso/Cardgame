7121af7dd7d64e2be5b24ca45342a1676eff8920
Joseph Loic Bisso-Bi-Ela <j.bisso-bi-ela@tu-braunschweig.de>
2021-04-06 01:49:33 +0200

    Methode für Klasse Karte weitergeschrieben

--- app/src/main/java/Cardgame/Karte.java
+++ app/src/main/java/Cardgame/Karte.java
@@ -1,17 +1,18 @@
 package Cardgame;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Karte {
 
-    private String wert;
-    private String farbe;
-    private String motiv;
+    private final String WERT;
+    private final String farbe;
+    private final String motiv;
     private ArrayList<String> rules;
     private int anzahl;
     private String style;
 
-    public Karte(String wert, String farbe, String motiv) throws SpielException {
+    public Karte(String WERT, String farbe, String motiv) throws SpielException {
 
         if (farbe.equals("rot") && (motiv.equals("herz") || motiv.equals("losange"))) {
             this.farbe = farbe;
@@ -32,12 +33,12 @@ public class Karte {
         } else {
             throw new SpielException("Karte: Farbe passt nicht zum Motiv");
         }
-        this.wert = wert;
-        rules = new ArrayList<String>();
+        this.WERT = WERT;
+        rules = new ArrayList<>();
     }
 
-    public String getWert() {
-        return wert;
+    public String getWERT() {
+        return WERT;
     }
 
     public String getFarbe() {
@@ -53,19 +54,20 @@ public class Karte {
     }
 
     public String[] getRules() {
-        return rules.toArray(new String[rules.size()]);
+        return rules.toArray(new String[0]);
     }
 
     public String printRule() {
-        String regeln = "";
+        StringBuilder regeln = new StringBuilder("Rule for Card " + WERT + ";");
         String[] allRules = getRules();
         if (allRules.length == 0) {
-            return regeln;
+            return "";
         }
         for (String regel : getRules()) {
-            regeln += regel + ";";
+            regeln.append(regel);
+            regeln.append(";");
         }
-        return regeln;
+        return regeln.toString();
     }
 
     public int getAnzahl() {
@@ -80,4 +82,39 @@ public class Karte {
         return style;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Karte)) return false;
+        Karte karte = (Karte) o;
+        return Objects.equals(WERT, karte.WERT) && Objects.equals(farbe, karte.farbe) && Objects.equals(motiv, karte.motiv);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(WERT, farbe, motiv);
+    }
+
+    public boolean hasRule() {
+        return rules.size() != 0;
+    }
+
+    public int compareTo(Object o)  {
+        if (this == o) return 0;
+        try {
+            Karte card = (Karte) o;
+            if (Character.isDigit(this.WERT.charAt(0)) && Character.isLetter(card.getWERT().charAt(0))){
+                return -1;
+            } else if (Character.isLetter(this.WERT.charAt(0)) && Character.isDigit(card.getWERT().charAt(0))){
+                return 1;
+            } else if (Character.isLetterOrDigit(this.WERT.charAt(0)) && Character.isLetterOrDigit(card.getWERT().charAt(0))) {
+                return Character.compare(this.WERT.charAt(0), card.getWERT().charAt(0));
+            }
+        } catch (NullPointerException e) {
+            return 1;
+        } catch (ClassCastException e) {
+            return 0;
+        }
+        return 0;
+    }
 }
