8a431b27e2af6694e9869e52172240ce43e933a6
Joseph Loic Bisso-Bi-Ela <j.bisso-bi-ela@tu-braunschweig.de>
2021-04-12 03:04:04 +0200

    Main Methode des Spiels geschrieben

---
+++ app/src/main/java/Cardgame/Main.java
@@ -0,0 +1,10 @@
+package Cardgame;
+
+import Gui.GUIException;
+import Gui.StartScreen;
+
+public class Main {
+    public static void main(String[] args) {
+        StartScreen startScreen = new StartScreen();
+    }
+}
