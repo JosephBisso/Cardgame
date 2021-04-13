bfd658a131b4f2cc5f6aba557eb74c6d16c2e40b
Joseph Loic Bisso-Bi-Ela <j.bisso-bi-ela@tu-braunschweig.de>
2021-04-14 01:55:44 +0200

    Gameplay GUI weiterbearbeitet. Methode zum SPielen ergänzt. FEHLT: Runden, AI, Tabs, Double Click

--- app/src/main/java/Gui/GamePlayScreen.java
+++ app/src/main/java/Gui/GamePlayScreen.java
@@ -1,6 +1,7 @@
 package Gui;
 
 import Cardgame.Deck;
+import Cardgame.Karte;
 import Cardgame.Players;
 import Cardgame.Spieler;
 
@@ -9,6 +10,8 @@ import javax.swing.border.TitledBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 
 public class GamePlayScreen extends JFrame {
@@ -23,19 +26,7 @@ public class GamePlayScreen extends JFrame {
     private JPanel Table_cards;
     private JLabel label_tableCards;
     private JButton pickButton;
-    private JPanel panel_yourCards;
     private JPanel panel_yourCardIMG;
-    private JLabel label_listCard0;
-    private JLabel label_listCard1;
-    private JLabel label_listCard2;
-    private JLabel label_listCard3;
-    private JLabel label_listCard4;
-    private JLabel label_listCard5;
-    private JLabel label_listCard6;
-    private JLabel label_listCard7;
-    private JLabel label_listCard8;
-    private JLabel label_listCard9;
-    private JScrollBar scrollBar1;
     private JLabel label_CardIMG0;
     private JLabel label_CardIMG1;
     private JLabel label_CardIMG2;
@@ -46,10 +37,49 @@ public class GamePlayScreen extends JFrame {
     private JLabel label_CardIMG7;
     private JLabel label_CardIMG8;
     private JLabel label_CardIMG9;
+    private JButton playSelectedCardButton;
+    private JList list_yourdCards;
+    private JTextArea label_addedCards;
+    private JLabel label_Card0;
+    private JLabel label_Card1;
+    private JLabel label_Card2;
+    private JLabel label_Card3;
+    private JLabel label_Card4;
+    private JLabel label_Card5;
+    private JLabel label_Card6;
+    private JLabel label_Card7;
+    private JLabel label_Card8;
+    private JLabel label_Card9;
+    private JButton resetButton;
+    private JLabel label_Card10;
+    private JLabel label_Card11;
+    private JLabel label_Card12;
+    private JLabel label_Card13;
+    private JLabel label_Card14;
+    private JLabel label_Card15;
+    private JLabel label_Card16;
+    private JLabel label_Card17;
+    private JLabel label_Card18;
+    private JLabel label_Card19;
+    private JTextArea textArea1;
+    private JTabbedPane tabbedPane_yourCards;
+    private JPanel panel_scrollbar;
+
+    private JLabel[] label_yourCardsIMG = {label_CardIMG0, label_CardIMG1, label_CardIMG2, label_CardIMG3,label_CardIMG4,
+            label_CardIMG5, label_CardIMG6, label_CardIMG7, label_CardIMG8, label_CardIMG9};
+    private JLabel[] label_yourCards = {label_Card0, label_Card1, label_Card2, label_Card3, label_Card4, label_Card5,
+            label_Card6, label_Card7, label_Card8, label_Card9, label_Card10, label_Card11, label_Card12, label_Card13, label_Card14, label_Card15,
+            label_Card16, label_Card17, label_Card18, label_Card19};
+    private Karte[] playerKarten;
+    private String cardsToBePlayed = "";
+    private ArrayList<Integer> index_cardsToBePlayed;
+    private ArrayList<Karte> arrayList_cardsToBePlayed;
 
     public GamePlayScreen(Deck deck) {
         this.deck = deck;
         AINamen = new ArrayList<>();
+        index_cardsToBePlayed = new ArrayList<>();
+        arrayList_cardsToBePlayed = new ArrayList<>();
         JFrame frame = new JFrame("GamePlayScreen");
         frame.setContentPane(mainPanel_Gameplay);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -58,6 +88,168 @@ public class GamePlayScreen extends JFrame {
         frame.setVisible(true);
 
         initializeGame();
+        label_CardIMG0.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.getClickCount() == 2) {
+                    cardsToBePlayed += playerKarten[0].print() + "\n";
+                    label_addedCards.setText(cardsToBePlayed);
+                    index_cardsToBePlayed.add(0);
+                }
+
+            }
+        });
+        label_CardIMG1.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.getClickCount() == 2) {
+                    cardsToBePlayed += playerKarten[1].print() + "\n";
+                    label_addedCards.setText(cardsToBePlayed);
+                    index_cardsToBePlayed.add(1);
+                }
+            }
+        });
+        resetButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cardsToBePlayed = "";
+                label_addedCards.setText(cardsToBePlayed);
+                index_cardsToBePlayed.clear();
+            }
+        });
+        label_CardIMG2.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.getClickCount() == 2) {
+                    cardsToBePlayed += playerKarten[2].print() + "\n";
+                    label_addedCards.setText(cardsToBePlayed);
+                    index_cardsToBePlayed.add(2);
+                }
+            }
+        });
+        label_CardIMG3.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.getClickCount() == 2) {
+                    cardsToBePlayed += playerKarten[3].print() + "\n";
+                    label_addedCards.setText(cardsToBePlayed);
+                    index_cardsToBePlayed.add(3);
+                }
+            }
+        });
+        label_CardIMG4.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.getClickCount() == 2) {
+                    cardsToBePlayed += playerKarten[4].print() + "\n";
+                    label_addedCards.setText(cardsToBePlayed);
+                    index_cardsToBePlayed.add(4);
+                }
+            }
+        });
+        label_CardIMG5.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.getClickCount() == 2) {
+                    cardsToBePlayed += playerKarten[5].print() + "\n";
+                    label_addedCards.setText(cardsToBePlayed);
+                    index_cardsToBePlayed.add(5);
+                }
+            }
+        });
+        label_CardIMG6.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.getClickCount() == 2) {
+                    cardsToBePlayed += playerKarten[6].print() + "\n";
+                    label_addedCards.setText(cardsToBePlayed);
+                    index_cardsToBePlayed.add(6);
+                }
+            }
+        });
+        label_CardIMG7.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.getClickCount() == 2) {
+                    cardsToBePlayed += playerKarten[7].print() + "\n";
+                    label_addedCards.setText(cardsToBePlayed);
+                    index_cardsToBePlayed.add(7);
+                }
+            }
+        });
+        label_CardIMG8.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.getClickCount() == 2) {
+                    cardsToBePlayed += playerKarten[8].print() + "\n";
+                    label_addedCards.setText(cardsToBePlayed);
+                    index_cardsToBePlayed.add(8);
+                }
+            }
+        });
+        label_CardIMG9.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.getClickCount() == 2) {
+                    cardsToBePlayed += playerKarten[9].print() + "\n";
+                    label_addedCards.setText(cardsToBePlayed);
+                    index_cardsToBePlayed.add(9);
+                }
+            }
+        });
+        pickButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                player.pick(1);
+                setLabels(player.getCards());
+            }
+        });
+        playSelectedCardButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                playerPlaysSelectedCards();
+            }
+        });
+        label_Card6.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.getClickCount() == 2) {
+                    cardsToBePlayed += playerKarten[6].print() + "\n";
+                    label_addedCards.setText(cardsToBePlayed);
+                    index_cardsToBePlayed.add(6);
+                }
+            }
+        });
+        label_Card7.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.getClickCount() == 2) {
+                    cardsToBePlayed += playerKarten[7].print() + "\n";
+                    label_addedCards.setText(cardsToBePlayed);
+                    index_cardsToBePlayed.add(7);
+                }
+            }
+        });
+        label_Card8.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.getClickCount() == 2) {
+                    cardsToBePlayed += playerKarten[8].print() + "\n";
+                    label_addedCards.setText(cardsToBePlayed);
+                    index_cardsToBePlayed.add(8);
+                }
+            }
+        });
+        label_Card9.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.getClickCount() == 2) {
+                    cardsToBePlayed += playerKarten[9].print() + "\n";
+                    label_addedCards.setText(cardsToBePlayed);
+                    index_cardsToBePlayed.add(9);
+                }
+            }
+        });
     }
 
     public void initializeGame() {
@@ -73,17 +265,9 @@ public class GamePlayScreen extends JFrame {
         numberCardPlayer1.setText(Integer.toString(getAI(AINamen.get(0)).getAnzahlCards()));
         numberCardPlayer2.setText(Integer.toString(getAI(AINamen.get(1)).getAnzahlCards()));
         numberCardPlayer3.setText(Integer.toString(getAI(AINamen.get(2)).getAnzahlCards()));
-        label_tableCards.setIcon(deck.getTopCard().getStyle());
+        setIconTableCard();
 
-        JLabel[] label_yourCards = {label_CardIMG0, label_CardIMG1, label_CardIMG2, label_CardIMG3,label_CardIMG4,
-                    label_CardIMG5, label_CardIMG6, label_CardIMG7, label_CardIMG8, label_CardIMG9},
-                label = {label_listCard0, label_listCard1, label_listCard2, label_listCard3, label_listCard4,
-                label_listCard5, label_listCard6, label_listCard7, label_listCard8, label_listCard9};
-        for (int i = 0; i < player.getAnzahlCards(); i++) {
-            label[i].setText(player.getCards()[i].print());
-            //label.setEnabled(false);
-            label_yourCards[i].setIcon(player.getCards()[i].getStyle());
-        }
+        setLabels(player.getCards());
         panel_yourCardIMG.setAutoscrolls(true);
     }
 
@@ -94,4 +278,80 @@ public class GamePlayScreen extends JFrame {
         return null;
     }
 
+    private void setLabels(Karte[] karten) {
+        playerKarten = karten;
+        for(int i = 0; i < karten.length; i++) {
+            if (!label_yourCards[i].isEnabled()) label_yourCards[i].setEnabled(true);
+            label_yourCards[i].setText(karten[i].print());
+            if (i < label_yourCardsIMG.length) {
+                if (!label_yourCardsIMG[i].isVisible()) label_yourCardsIMG[i].setVisible(true);
+                label_yourCardsIMG[i].setIcon(karten[i].getStyle());
+            }
+        }
+        if (karten.length < label_yourCards.length) {
+            for (int j = karten.length ; j < label_yourCards.length; j++) {
+                label_yourCards[j].setText("none");
+                label_yourCards[j].setEnabled(false);
+                if (j < label_yourCardsIMG.length) {
+                    label_yourCardsIMG[j].setVisible(false);
+                }
+
+            }
+        }
+    }
+
+    private void setIconTableCard () {
+        if (deck.getPlayedCards().size() == 0) {
+            label_tableCards.setIcon(deck.giveOneCard().getStyle());
+        }
+        label_tableCards.setIcon(deck.getLasPlayedCard().getStyle());
+    }
+
+    private void playerPlaysSelectedCards() {
+        if (index_cardsToBePlayed.isEmpty()) {
+            JOptionPane.showMessageDialog(null,"No Cards have been " +
+                            "selected. Pick a Card, if you want to skip playing!",
+                    "Error", JOptionPane.WARNING_MESSAGE);
+            return;
+        }
+        for (int i : index_cardsToBePlayed) {
+            arrayList_cardsToBePlayed.add(playerKarten[i]);
+        }
+        for (int i = 0; i < arrayList_cardsToBePlayed.size() - 1; i++) {
+            if (!arrayList_cardsToBePlayed.get(i).matchesForPlay(arrayList_cardsToBePlayed.get(i + 1))) {
+                JOptionPane.showMessageDialog(null,
+                        arrayList_cardsToBePlayed.get(i).print() +
+                                "\ncan not be combined-played with\n" +
+                                arrayList_cardsToBePlayed.get(i + 1).print(),
+                        "Error", JOptionPane.WARNING_MESSAGE);
+                cardsToBePlayed = "";
+                label_addedCards.setText(cardsToBePlayed);
+                index_cardsToBePlayed.clear();
+                arrayList_cardsToBePlayed.clear();
+                return;
+            }
+        }
+        if (!deck.getLasPlayedCard().matches(arrayList_cardsToBePlayed.get(0))) {
+            JOptionPane.showMessageDialog(null,
+                    arrayList_cardsToBePlayed.get(0).print() +
+                            "\ndoes not matches with\n" +
+                            deck.getLasPlayedCard().print() + "\nOn the Table",
+                    "Error", JOptionPane.WARNING_MESSAGE);
+            cardsToBePlayed = "";
+            label_addedCards.setText(cardsToBePlayed);
+            index_cardsToBePlayed.clear();
+            arrayList_cardsToBePlayed.clear();
+            return;
+        }
+        for (int i : index_cardsToBePlayed) {
+            label_yourCardsIMG[i].setIcon(new ImageIcon());
+        }
+        player.play(arrayList_cardsToBePlayed.toArray(new Karte[0]));
+        cardsToBePlayed = "";
+        label_addedCards.setText(cardsToBePlayed);
+        index_cardsToBePlayed.clear();
+        arrayList_cardsToBePlayed.clear();
+        setLabels(player.getCards());
+        setIconTableCard();
+    }
 }
